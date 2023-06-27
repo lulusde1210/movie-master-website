6ea645582cd113b4fd7beb0b7428f1d0138c863f
lulu <lulusun1210@gmail.com>
2023-06-27 12:29:21 -0500

    fix typo

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    #     "SQLALCHEMY_DATABASE_URI")
+    #     "SQLALCHEMY_DATABASE_URL")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
         "RENDER_DATABASE_URI")
     app.config['SECRET_KEY'] = os.environ.get(

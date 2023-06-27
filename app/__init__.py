f0da112767afcda884f3676b1e84a2c68ee928c0
lulu <lulusun1210@gmail.com>
2023-06-27 11:28:35 -0500

    change database url for deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI")
+        "DATABASE_URI")
     app.config['SECRET_KEY'] = os.environ.get(
         "SECRET_KEY")
 
